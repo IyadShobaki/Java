860390f47fe08724103a968763a30d2532208834
Iyad Shobaki <iyadshobaki82@gmail.com>
2019-01-20 13:26:44 -0500

    Complete wwek 2 lesson

---
+++ MyProject/src/Welocme.java
@@ -0,0 +1,20 @@
+
+public class Welocme {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("Welcome to Java Programming!");
+		
+//		System.out.print("Welcome to ");
+//		System.out.println("Java Programming");
+//		System.out.println("Welcome\nto\nJava\nProgramming!");
+		System.out.printf("%s%n%s%n%d", "Welcome to", "Java Programming!", 5);
+		
+		System.out.println(1 + " " +  2  + " " +  3 + " " + 4);	
+		
+		System.out.print(1 + " " + 2 + " " + 3 + " "+ 4 + "\n");
+		
+		System.out.printf("%d%s%d%s%d%s%d", 1," ",2," ",3," ",4);
+	}
+
+}
---
+++ Numbers/src/Numbers.java
@@ -0,0 +1,19 @@
+/*
+ 	Iyad Shobaki
+    SPRING 2019 Java Programming
+    Professor Sharon Hoover
+    Lab 2
+    1/20/2019
+ */
+public class Numbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(1 + " " +  2  + " " +  3 + " " + 4);	
+		
+		System.out.print(1 + " " + 2 + " " + 3 + " "+ 4 + "\n");
+		
+		System.out.printf("%d%s%d%s%d%s%d", 1," ",2," ",3," ",4);
+	}
+
+}
